be4f01a82399800794e9c24d4e9ca2dd46bd95fd
Oliver Heilig <oliver.heilig@yahoo.de>
2022-04-26 18:40:45 +0200

    fix for Leaflet 1.8.0

--- src/TileLayer.XServer.js
+++ src/TileLayer.XServer.js
@@ -25,10 +25,8 @@ L.TileLayer.XServer = L.TileLayer.extend({
 		L.TileLayer.prototype.onAdd.call(this, map);
 
 		if(!this.options.disableMouseEvents) {
-			var cont = map._container;
-
-			cont.addEventListener('mousemove', L.bind(this._onMouseMove, this), true);
-			cont.addEventListener('mousedown', L.bind(this._onMouseDown, this), true);
+			map._container.addEventListener('mousemove', L.bind(this._onMouseMove, this), true);
+			map._mapPane.addEventListener('mousedown', L.bind(this._onMouseDown, this), true);
 
 			map._mapPane.addEventListener('click', L.bind(this._onClick, this), true);
 			map.addEventListener('click', L.bind(this._onMapClick, this), false);
@@ -37,11 +35,9 @@ L.TileLayer.XServer = L.TileLayer.extend({
 
 	onRemove: function (map) {
 		if(!this.options.disableMouseEvents) {
-			var cont = map._container;
-
-			cont.removeEventListener('mousemove', L.bind(this._onMouseMove, this), true);
-			cont.removeEventListener('mousedown', L.bind(this._onMouseDown, this), true);
-
+			map._container.removeEventListener('mousemove', L.bind(this._onMouseMove, this), true);
+			map._mapPane.removeEventListener('mousedown', L.bind(this._onMouseDown, this), true);
+  
 			map._mapPane.removeEventListener('click', L.bind(this._onClick, this), true);
 			map.removeEventListener('click', L.bind(this._onMapClick, this), false);
 		}
